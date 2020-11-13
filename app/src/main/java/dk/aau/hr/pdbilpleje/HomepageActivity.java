e1953ac4f7167dd4811a65b7ad6ae04abdecf8b9
nato93 <44198423+nato93@users.noreply.github.com>
2020-11-13 15:15:51 +0100

    Created login actiity and homepage

---
+++ app/src/main/java/dk/aau/hr/pdbilpleje/HomepageActivity.java
@@ -0,0 +1,28 @@
+package dk.aau.hr.pdbilpleje;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomepageActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+
+        Toast.makeText(this, "It worked mothersucka!", Toast.LENGTH_SHORT).show();
+
+
+
+
+
+    }
+
+
+
+
+}
