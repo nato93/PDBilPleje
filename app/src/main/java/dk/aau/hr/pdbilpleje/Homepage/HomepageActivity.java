eabb795edad824123500279ba6ed1e97f7347ff1
nato93 <44198423+nato93@users.noreply.github.com>
2020-11-17 16:57:46 +0100

    Created login actiity and homepage

--- app/src/main/java/dk/aau/hr/pdbilpleje/Homepage/HomepageActivity.java
+++ app/src/main/java/dk/aau/hr/pdbilpleje/Homepage/HomepageActivity.java
@@ -1,9 +1,12 @@
-package dk.aau.hr.pdbilpleje;
+package dk.aau.hr.pdbilpleje.Homepage;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import dk.aau.hr.pdbilpleje.R;
 
 public class HomepageActivity extends AppCompatActivity {
 
@@ -12,6 +15,12 @@ public class HomepageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction()
+                .add(R.id.mainPageLayout, new HomepageFragment())
+                .commit();
+
+
 
 
         Toast.makeText(this, "It worked mothersucka!", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/dk/aau/hr/pdbilpleje/Homepage/HomepageFragment.java
@@ -0,0 +1,44 @@
+package dk.aau.hr.pdbilpleje.Homepage;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+import dk.aau.hr.pdbilpleje.R;
+
+public class HomepageFragment extends Fragment {
+
+    public ImageView mNewsImageView;
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_homepage, container, false);
+
+        mNewsImageView = view.findViewById(R.id.newsImageview);
+
+
+
+
+        //gaugeInfoText = view.findViewById(R.id.mInfoText);
+
+        //gaugeInfoText = MainPageFragment.infoText;
+
+        //CharSequence input = gaugeText.toString();
+        //updateTextView(input);
+
+        //gaugeText.setText(R.string.mCoInfo);
+        //CharSequence input = gaugeText.getText();
+        //updateTextView(gaugeText.toString());
+        return view;
+    }
+
+}
