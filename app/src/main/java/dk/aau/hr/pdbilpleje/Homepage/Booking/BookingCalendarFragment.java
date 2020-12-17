88713b802167c2e87febf1b3fc87f1b5f14e6fdb
Hamad Rafi <hamadiqbal46@gmail.com>
2020-12-17 11:55:01 +0100

    booking

--- app/src/main/java/dk/aau/hr/pdbilpleje/Homepage/Booking/BookingCalendarFragment.java
+++ app/src/main/java/dk/aau/hr/pdbilpleje/Homepage/Booking/BookingCalendarFragment.java
@@ -1,24 +1,32 @@
 package dk.aau.hr.pdbilpleje.Homepage.Booking;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.gms.maps.MapView;
+import com.google.android.material.datepicker.CalendarConstraints;
+import com.google.android.material.datepicker.DateValidatorPointForward;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
 import dk.aau.hr.pdbilpleje.R;
 
 public class BookingCalendarFragment extends Fragment {
 
 
-    public MapView mMapView;
-    private Button mBookingNextButton, mBookingPreviousButton;
+    private Button mBookingNextButton, mBookingPreviousButton, mChooseDateButton;
+    private TextView mTextViewDate;
+
 
 
 
@@ -27,13 +35,37 @@ public class BookingCalendarFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_bookingcalendar, container, false);
 
+        MaterialDatePicker.Builder materialDateBuilder = MaterialDatePicker.Builder.datePicker();
+        materialDateBuilder.setTitleText("VÆLG EN DATO");
+        CalendarConstraints.Builder calendarConstraintBuilder = new CalendarConstraints.Builder();
+        calendarConstraintBuilder.setValidator(DateValidatorPointForward.now());
+        materialDateBuilder.setCalendarConstraints(calendarConstraintBuilder.build());
+        final MaterialDatePicker materialDatePicker = materialDateBuilder.build();
 
-        mMapView = view.findViewById(R.id.bookingMapView);
-        mBookingNextButton = view.findViewById(R.id.bookingNextButton);
-        mBookingPreviousButton = view.findViewById(R.id.bookingPreviousButton);
+        mTextViewDate = view.findViewById(R.id.textViewDate);
+        mChooseDateButton = view.findViewById(R.id.addDateButton);
+        mBookingNextButton = view.findViewById(R.id.bookingNextButton2);
+        mBookingPreviousButton = view.findViewById(R.id.bookingPreviousButton2);
 
 
-        mBookingPreviousButton.setOnClickListener(new View.OnClickListener() {
+        mChooseDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialDatePicker.show(getFragmentManager(), "MATERIAL_DATE_PICKER");
+            }
+        });
+
+        materialDatePicker.addOnPositiveButtonClickListener(
+                new MaterialPickerOnPositiveButtonClickListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onPositiveButtonClick(Object selection) {
+
+                        mTextViewDate.setText(materialDatePicker.getHeaderText());
+                    }
+                });
+
+       mBookingPreviousButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 BookingWhereFragment bookingWhereFragment= new BookingWhereFragment();
--- app/src/main/java/dk/aau/hr/pdbilpleje/Homepage/Booking/BookingFragment.java
+++ app/src/main/java/dk/aau/hr/pdbilpleje/Homepage/Booking/BookingFragment.java
@@ -1,21 +1,32 @@
 package dk.aau.hr.pdbilpleje.Homepage.Booking;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.EditText;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+
+import java.util.ArrayList;
 
 import dk.aau.hr.pdbilpleje.R;
 
 
 public class BookingFragment extends Fragment {
 
-    public Spinner mServiceSpinner, mCarSpinner;
-    public Button mBookingNextButton;
+   // public Spinner mServiceSpinner, mCarSpinner;
+    public Button mBookingNextButton, mAddCarButton;
+    private AlertDialog.Builder dialogBuilder, dialogBuilder2;
+    private AlertDialog dialog, dialog2;
+    private ArrayList <String> cars;
+    private EditText mEditTextModel;
 
 
     @Override
@@ -24,11 +35,41 @@ public class BookingFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_booking, container, false);
 
-        mServiceSpinner = view.findViewById(R.id.spinnerServiceView);
-        mCarSpinner = view.findViewById(R.id.spinnerCarView);
-        mBookingNextButton = view.findViewById(R.id.bookingNextButton);
+        String[] services = new String[] {"CAP WASH", "NANO WASH", "BIG CAP WASH", "NORMAL WASH"};
+        cars = new ArrayList<>();
+        cars.add("Lambocap");
+        cars.add("Toyota Corolla");
+        mBookingNextButton = view.findViewById(R.id.NæsteButton);
+        mAddCarButton = view.findViewById(R.id.addCarButton);
 
+        mAddCarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewCarDialog();
+            }
+        });
 
+      //  mNextButtonPopup = view.findViewById(R.id.nextButtonPopup);
+
+        ArrayAdapter<String> service =
+                new ArrayAdapter<>(
+                        getContext(),
+                        R.layout.dropdown_menu_popup_item,
+                        services);
+
+        AutoCompleteTextView editTextFilledExposedDropdownServices =
+                view.findViewById(R.id.filled_exposed_dropdown);
+        editTextFilledExposedDropdownServices.setAdapter(service);
+
+        ArrayAdapter<String> car =
+                new ArrayAdapter<>(
+                        getContext(),
+                        R.layout.dropdown_menu_popup_item,
+                        cars);
+
+        AutoCompleteTextView editTextFilledExposedDropdownCars =
+                view.findViewById(R.id.filled_exposed_dropdown2);
+        editTextFilledExposedDropdownCars.setAdapter(car);
 
         mBookingNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,4 +85,42 @@ public class BookingFragment extends Fragment {
         return view;
 
     } // OnCreateView End
+
+    public void createNewCarDialog(){
+        dialogBuilder = new AlertDialog.Builder(getContext());
+        final View newCarPopup = getLayoutInflater().inflate(R.layout.popup_booking, null);
+        dialogBuilder.setView(newCarPopup);
+        dialogBuilder.setPositiveButton("NÆSTE >", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                createNewCarDialog2();
+            }
+        });
+        dialog = dialogBuilder.create();
+        dialog.show();
+    }
+
+    public void createNewCarDialog2(){
+        dialogBuilder2 = new AlertDialog.Builder(getContext());
+        final View newCarPopup2 = getLayoutInflater().inflate(R.layout.popup2_booking,null);
+        dialogBuilder2.setView(newCarPopup2);
+        dialogBuilder2.setPositiveButton("TILFØJ BIL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                cars.add(mEditTextModel.getText().toString());
+            }
+        });
+
+        dialogBuilder2.setNegativeButton("TILBAGE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                createNewCarDialog();
+            }
+        });
+        dialog2 = dialogBuilder2.create();
+        dialog2.show();
+        mEditTextModel = dialog2.findViewById(R.id.editTextCarmodel);
+
+    }
 } //Class End
