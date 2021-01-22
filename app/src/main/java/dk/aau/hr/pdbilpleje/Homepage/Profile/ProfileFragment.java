2b4c5a62e39b38510a82e653c58661f863ef741a
Hamad Rafi <hamadiqbal46@gmail.com>
2021-01-22 11:16:36 +0100

    PROFILE

--- app/src/main/java/dk/aau/hr/pdbilpleje/Homepage/Profile/ProfileFragment.java
+++ app/src/main/java/dk/aau/hr/pdbilpleje/Homepage/Profile/ProfileFragment.java
@@ -1,6 +1,8 @@
 package dk.aau.hr.pdbilpleje.Homepage.Profile;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,8 +25,11 @@ public class ProfileFragment extends Fragment {
 
     private TextView mProfileName;
     private FirebaseAuth firebaseAuth;
+    public Button mChangeNameButton, mChangePasswordButton;
+    private AlertDialog.Builder dialogBuilder, dialogBuilder2;
+    private AlertDialog dialog, dialog2;
 
-    public ProfileFragment() {
+   /* public ProfileFragment() {
         // Required empty public constructor
     }
 
@@ -34,7 +40,7 @@ public class ProfileFragment extends Fragment {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = firebaseAuth.getCurrentUser();
         //updateUI(currentUser);
-    }
+    }*/
 
 
 
@@ -46,11 +52,57 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         //mProfileName = view.findViewById(R.id.textViewProfileName);
+        mChangeNameButton = view.findViewById(R.id.changeNameButton);
+        mChangePasswordButton= view.findViewById(R.id.changePasswordButton);
+
+        mChangeNameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewCarDialog();
+            }
+        });
 
 
-
+        mChangePasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewCarDialog();
+            }
+        });
 
         return view;
+
+    }
+
+    public void createNewCarDialog(){
+        dialogBuilder = new AlertDialog.Builder(getContext());
+        final View newCarPopup = getLayoutInflater().inflate(R.layout.popup_profile_name, null);
+        dialogBuilder.setView(newCarPopup);
+        dialogBuilder.setPositiveButton("BEKRÆFT", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                createNewCarDialog2();
+            }
+        });
+        dialog = dialogBuilder.create();
+        dialog.show();
+    }
+
+    public void createNewCarDialog2(){
+        dialogBuilder2 = new AlertDialog.Builder(getContext());
+        final View newCarPopup2 = getLayoutInflater().inflate(R.layout.popup_profile_password,null);
+        dialogBuilder2.setView(newCarPopup2);
+        dialogBuilder2.setPositiveButton("BEKRÆFT", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+             //   cars.add(mEditTextModel.getText().toString());
+            }
+        });
+
+        dialog2 = dialogBuilder2.create();
+        dialog2.show();
+       // mEditTextModel = dialog2.findViewById(R.id.editTextCarmodel);
 
     }
 
